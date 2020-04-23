50dd65a0413c62217c5174e403aec1bff91edfde
Poon Tsz Kin <alvispoon@gmail.com>
2020-04-23 15:55:46 +0800

    Initial Commit

---
+++ LearnChineseEveryday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnChineseEveryday
+//
+//  Created by Alvis Poon on 23/4/2020.
+//  Copyright © 2020 Alvis Poon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
