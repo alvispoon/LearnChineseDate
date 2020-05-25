260029b13e33a8aa3bc7f6433cc0e168a7db7690
Poon Tsz Kin <alvispoon@gmail.com>
2020-05-25 17:54:17 +0800

    Updated

--- LearnChineseEveryday/ViewController.swift
+++ LearnChineseEveryday/ViewController.swift
@@ -63,6 +63,28 @@ class ViewController: UIViewController{
         let speakgesture = UITapGestureRecognizer(target: self, action: #selector(speakDate))
          speakerImage.addGestureRecognizer(speakgesture)
 
+        
+        registerSettingsBundle()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.defaultsChanged), name: UserDefaults.didChangeNotification, object: nil)
+        defaultsChanged()
+    }
+    
+    func registerSettingsBundle(){
+        let appDefaults = [String:AnyObject]()
+        UserDefaults.standard.register(defaults: appDefaults)
+    }
+    @objc func defaultsChanged(){
+        if UserDefaults.standard.bool(forKey: "RedThemeKey") {
+            self.view.backgroundColor = UIColor.red
+        
+        }
+        else {
+            self.view.backgroundColor = UIColor.green
+        }
+    }
+    
+    deinit { //Not needed for iOS9 and above. ARC deals with the observer in higher versions.
+        NotificationCenter.default.removeObserver(self)
     }
     
     
